d9454d851749bba6826c0995655de9451459cbff
Vitaliy Ishchuk <vishchuk@softjourn.com>
2014-05-12 10:52:33 +0300

    Added execute/cancel intents.

--- src/com/example/framework/service/AppServiceHelperImpl.java
+++ src/com/example/framework/service/AppServiceHelperImpl.java
@@ -1,10 +1,36 @@
 package com.example.framework.service;
 
-public class AppServiceHelperImpl implements AppServiceHelper{
+import android.app.Application;
+import android.content.Intent;
+
+import com.example.framework.processor.BaseProcessor;
+
+public class AppServiceHelperImpl implements AppServiceHelper {
+
+	private Application mApp;
+
+	public AppServiceHelperImpl(Application app) {
+		mApp = app;
+	}
 
 	@Override
 	public void exampleProcess() {
 
 	}
 
+	private Intent createCancelableServiceIntent(int processorId) {
+		Intent intent = new Intent(mApp, AppService.class);
+		intent.setAction(AppService.ACTION_CANCEL_PROCESS);
+		intent.putExtra(AppService.EXTRA_PROCESSOR_ID, processorId);
+		return intent;
+	}
+
+	private Intent createExecutableServiceIntent(BaseProcessor baseProcessor,
+			int processorId) {
+		Intent intent = new Intent(mApp, AppService.class);
+		intent.setAction(AppService.ACTION_EXECUTE_PROCESS);
+		intent.putExtra(AppService.EXTRA_PROCESSOR, baseProcessor);
+		intent.putExtra(AppService.EXTRA_PROCESSOR_ID, processorId);
+		return intent;
+	}
 }
